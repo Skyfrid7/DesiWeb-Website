8adb061b5ae0e4e1b44dab0c20fd228178c04e40
Skyfrid7 <skyfrid.webdev@gmail.com>
2021-06-19 14:56:15 +0200

    mail markdown fix

--- resources/views/emails/contact/contact-form.blade.php
+++ resources/views/emails/contact/contact-form.blade.php
@@ -1,11 +1,11 @@
 @component('mail::message')
 # Bonjour
 
-Vous avez reçu un mail de la part de {{ $data['first-name'] . $data['name'] }}
-<b>Email:</b> {{ $data['email'] }}
+Vous avez reçu un mail de la part de {{ $data['first-name'] . ' ' . $data['name'] }}<br>
+<b>Email:</b> {{ $data['email'] }}<br>
 <b>Téléphone:</b> {{ $data['tel'] }}
 
-<b>Sujet:</b> {{ $data['subject'] }}
+<b>Sujet:</b> {{ $data['subject'] }}<br>
 <b>Message:</b> {{ $data['message'] }}
 
 Merci,<br>
