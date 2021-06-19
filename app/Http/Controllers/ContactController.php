9c18b327a6f357ce8f3fe1361048d47309197f63
Skyfrid7 <skyfrid.webdev@gmail.com>
2021-06-19 18:20:22 +0200

    help

--- app/Http/Controllers/ContactController.php
+++ app/Http/Controllers/ContactController.php
@@ -24,7 +24,7 @@ class ContactController extends Controller
             'message' => 'required'
         ]);
 
-        Mail::to('contact@desiweb.fr')->send(new ContactMail($data));
+        Mail::to('skyfrid.webdev@gmail.com')->send(new ContactMail($data));
 
         return redirect('contact')->with('message', 'Votre message a bien été envoyé.');
     }
