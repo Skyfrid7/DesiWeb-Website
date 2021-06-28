997928061338510b247d54b6722786873378f8ab
Skyfrid7 <skyfrid.webdev@gmail.com>
2021-06-28 19:40:40 +0200

    wave fix

--- resources/views/services.blade.php
+++ resources/views/services.blade.php
@@ -19,8 +19,6 @@
           <img src="{{ asset('assets/img/services-img.png') }}" alt="image d'une application en construction">
      </div>
 
-     <video src="{{ asset('assets/vid/whitebars.mp4') }}" muted preload="none" loop autoplay></video>
-
 </section>
 
 <img src="{{ asset('assets/svg/wave.svg') }}" alt="vague de transition" class="wave" id="services-wave">
